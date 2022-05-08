db6f44fcad7dd6b69b43fe7eaca833e7f4bd79e8
Shahin Safarov <tungsten.rust@gmail.com>
2022-05-08 03:46:42 -0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use sha1::Digest;
+use std::{
+    env,
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+const SHA1_HEX_STRING_LENGTH: usize = 40;
+
+fn main() -> Result<(), Box<dyn Error>>{
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() !=3 {
+        println!("sha1-sha_decrypter: <wordslist> <sha1-hash>");
+        return Ok(());
+    }
+    let hash_to_crack = args[2].trim();
+    if hash_to_crack.len() !=SHA1_HEX_STRING_LENGTH {
+        return Err("sha1 is not valid".into());
+    }
+    let wordlist_file = File::open(&args[1])?;
+    let reader = BufReader::new(&wordlist_file);
+
+    for line in reader.lines() {
+        let line = line?;
+        let common_password = line.trim();
+        if hash_to_crack ==
+            &hex::encode(sha1::Sha1::digest(common_password.as_bytes())){
+            println!("Password found: {}", &common_password);
+            return Ok(());
+        }
+        println!("password not found in wordlist");
+    }
+    Ok(())
+}
